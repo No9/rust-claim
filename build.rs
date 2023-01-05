5b925f6f7f864c8b91da99cb71a58a661a9fa23e
Anton Whalley <anton@venshare.com>
2023-01-05 14:48:29 +0000

    feature: disable autocfg with wasi
    Signed-off-by: Anton Whalley <anton@venshare.com>

--- build.rs
+++ build.rs
@@ -1,17 +1,17 @@
-// extern crate autocfg;
-
+extern crate autocfg;
+#[cfg(not(target_os = "wasi"))]
 fn main() {
-    // let cfg = autocfg::new();
-    // cfg.emit_path_cfg("core::task::Poll", "has_task_poll");
-    // cfg.emit_path_cfg("std::task::Poll", "has_task_poll");
+    let cfg = autocfg::new();
+    cfg.emit_path_cfg("core::task::Poll", "has_task_poll");
+    cfg.emit_path_cfg("std::task::Poll", "has_task_poll");
 
-    // // Needed to enable `#![no_std]` only on rustc versions that support it (rustc 1.6.0 and up).
-    // cfg.emit_rustc_version(1, 6);
+    // Needed to enable `#![no_std]` only on rustc versions that support it (rustc 1.6.0 and up).
+    cfg.emit_rustc_version(1, 6);
 
-    // // Needed for `assert_matches!`' minimum rust version.
-    // cfg.emit_rustc_version(1, 26);
+    // Needed for `assert_matches!`' minimum rust version.
+    cfg.emit_rustc_version(1, 26);
 
-    // if cfg.probe_rustc_version(1, 15) && !cfg.probe_rustc_version(1, 16) {
-    //     autocfg::emit("has_private_in_public_issue");
-    // }
+    if cfg.probe_rustc_version(1, 15) && !cfg.probe_rustc_version(1, 16) {
+        autocfg::emit("has_private_in_public_issue");
+    }
 }
